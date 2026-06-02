ba7f158bc04ecc53cb2f966bad11f836c47a3e66
Vincentxvx <v.r.rasch@icloud.com>
2026-06-02 19:17:57 +0200

    context és protectedroute kész

---
+++ react-final-project-RV/src/components/ProtectedRoute.jsx
@@ -0,0 +1,12 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/loginContext";
+
+const ProtectedRoute = ({ children}) => {
+    const { isLogged } = useAuth();
+
+    if (!isLogged) {
+        return <Navigate to='/' replace />
+    }
+
+    return children;
+}
